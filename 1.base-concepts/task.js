d698ace09b079b13bd752cf0181bb24e680d845d
EdmartEmpty <edmart666@gmail.com>
2025-04-17 14:44:59 +0300

    1.base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = (b**2) - 4*a*c;
+    console.log(discriminant);
+  if (discriminant < 0){ 
+    return arr;
+  };
+  if (discriminant === 0){ 
+   arr[0] =  -b/(2*a);
+   return arr;
+  };
+if (discriminant > 0){
+  arr[0] = (-b + Math.sqrt(discriminant) )/(2*a);
+  arr[1] = (-b - Math.sqrt(discriminant) )/(2*a);
   return arr;
+} 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let percentInMonths =  (percent/ 100) / 12;
+  let bodyCredit = amount - contribution;
+  let paymentPerMonth = bodyCredit * (percentInMonths + (percentInMonths / (((1 + percentInMonths)**countMonths) - 1)));
+  let result = paymentPerMonth*countMonths;
+  return +result.toFixed(2);
   
 }

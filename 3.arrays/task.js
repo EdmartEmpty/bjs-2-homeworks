06e09e8bb68fca84cd310c979656c73f9269090b
EdmartEmpty <edmart666@gmail.com>
2025-04-19 13:27:17 +0300

    getUsersNamesInAgeRanges from work

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,5 +3,11 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if(gender !== "мужской" && gender !== "женский"){
+        return 0;
+    }
+    if(users.length === 0){
+        return 0;
+    }
+   return  users.filter(x => x.gender === gender).map(x => x.age).reduce((x,y) => x + y) / users.filter(x => x.gender === gender).map(x => x.age).length
 }

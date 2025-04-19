9df3b0ecd47617068e12e93aa5f674cd60ff5bcf
EdmartEmpty <edmart666@gmail.com>
2025-04-19 15:28:04 +0300

    compareArrays edit arr + 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,7 @@ function compareArrays(arr1, arr2) {
    if(arr1.length > arr2.length || arr1.length < arr2.length){
         return false;
     } 
-   return arr.every((element,index) => element === arr2[index]);
+   return arr1.every((element,index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {

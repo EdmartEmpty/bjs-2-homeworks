3feb9ff1bc9b637ad535060feece0dcf65de22e1
EdmartEmpty <edmart666@gmail.com>
2025-04-19 19:21:42 +0300

    4.objects from work

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,25 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+    this.age = age;
+    this.male = male;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  this.marks = marks;
 }
 
 Student.prototype.getAverage = function () {
-  
+  return this.marks.reduce((x,y) => x+y)/this.marks.length;
+}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+   this.excluded = reason;
+    delete this.subject ;
+    delete this.marks ;
 }

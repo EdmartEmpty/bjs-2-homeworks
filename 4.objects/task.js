5b9ca214e63827e9c1e496a5b0b74f90b63923a7
Edmart <edmart666@gmail.com>
2025-04-22 10:02:51 +0300

    add func ?. & reduce(acc,element,index,arr version 2)

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,25 +10,12 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if(this?.marks){this.marks.push(...marks);}
-  // if(this.hasOwnProperty("marks")){
-  // this.marks.push(...marks);
-  //   }
-    
+  if(this?.marks){this.marks.push(...marks);}    
 }
 
 Student.prototype.getAverage = function () {
-    // if(this.hasOwnProperty("marks") && this.marks.length > 0){
-    //   let result = this.marks.reduce((x,y) => x+y)/this.marks.length;
-    //   return result;
-    // }
-    // if(this.hasOwnProperty("marks") && this.marks.length > 0){
-    //   let result = this.marks.reduce((acc,item,index,arr) => {acc+=item; if(index === arr.length-1){return acc/arr.length;}return acc;},0);
-    //   return result;
-    // }
      if(this?.marks){
-      let result = this.marks.reduce((acc,item,index,arr) => {acc+=item; if(index === arr.length-1){return acc/arr.length;}return acc;},0);
-        return result;
+        return this.marks.reduce((acc,item,index,arr) => {acc+=item; if(index === arr.length-1){return acc/arr.length;}return acc;},0);;
       }
     return 0;
 }

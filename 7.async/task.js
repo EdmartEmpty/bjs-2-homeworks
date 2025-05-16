db25da05a76560c5e09a74903d3737f63ac98e9b
EdmartEmpty <edmart666@gmail.com>
2025-05-16 10:45:53 +0300

    update async file

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,66 +1,63 @@
 class AlarmClock {
-    constructor(){
-       this.alarmCollection = [];
+    constructor() {
+        this.alarmCollection = [];
         this.intervalId = null;
     }
 
-    addClock(time,callback){
-        if (time === null || callback === undefined){
+    addClock(time, callback) {
+        if (!time || !callback) {
             throw new Error('Отсутствуют обязательные аргументы');
         }
-        for(let alarm of this.alarmCollection){
-            if(alarm.time === time){
-                console.warn('Уже присутствует звонок на это же время');
-               
-            }
+
+        if (this.alarmCollection.some(x => x.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
         }
-        let alarm = {time,callback,canCall:true};
+        let alarm = { time, callback, canCall: true };
         this.alarmCollection.push(alarm);
     }
-    removeClock(timeRemove){
-     this.alarmCollection = this.alarmCollection.filter(x => x.time !== timeRemove);
+    removeClock(timeRemove) {
+        this.alarmCollection = this.alarmCollection.filter(x => x.time !== timeRemove);
     }
 
-    getCurrentFormattedTime(){
+    getCurrentFormattedTime() {
         let timeNow = new Date();
-        return timeNow.toLocaleString("ru",{hour:"2-digit",minute:"2-digit"});
+        return timeNow.toLocaleString("ru", { hour: "2-digit", minute: "2-digit" });
     }
-    start(){
-        if(this.intervalId !== null){
+    start() {
+        if (this.intervalId !== null) {
             return;
         }
 
         let timeBinded = this.getCurrentFormattedTime.bind(this);
         const chek = () => this.alarmCollection.forEach(
-            function(x)
-            {if(x.time === timeBinded() && x.canCall === true)
-                {
-                x.callback();
-                x.canCall = false;
-    
-                 }
+            function (x) {
+                if (x.time === timeBinded() && x.canCall === true) {
+                    x.callback();
+                    x.canCall = false;
+
+                }
             });
-         
-        this.intervalId = setInterval(chek,1000);
-       
-       
+
+        this.intervalId = setInterval(chek, 1000);
+
+
+    }
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    };
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(x => x.canCall = true);
+    }
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
 }
-        stop(){
-            clearInterval(this.intervalId);
-            this.intervalId = null;
-                };
-            
-         resetAllCalls() {
-                    this.alarmCollection.forEach(x => x.canCall = true);
-                }
-        clearAlarms (){
-                this.stop();
-                this.alarmCollection = [];
-                }
-}
 
 
 
 
- 
- 
+
+
